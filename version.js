3c06de66966dc8937d658a7b4e9e12d3b4d255d5
fufudev <akmelf20@gmail.com>
2025-03-18 17:55:14 +0300

    feat: update

--- version.js
+++
@@ -1,6 +0,0 @@
-// Fetch version from public URL
-fetch('/VERSION')
-  .then(response => response.text())
-  .then(version => {
-    document.getElementById('app-title').textContent = `MyApp - v${version.trim()}`;
-  });
